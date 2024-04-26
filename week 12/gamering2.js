0b5c2429ae9439f7e0ee352dabfd1667513d85d0
MRmuyres <157074061+MRmuyres@users.noreply.github.com>
2024-04-26 17:52:00 -0600

    final & edits

--- week 12/gamering2.js
+++ week 12/gamering2.js
@@ -13,89 +13,102 @@ function setup() {
     movementy = Math.floor(Math.random() * 10) + 1;
 }
 
-function player(red, green, blue, playerx, playery,size){
+// Draw player
+function player(red, green, blue, playerx, playery, size) {
     fill(red, green, blue);
     noStroke();
     circle(playerx, playery, size);
 }
 
-
-function movingObstacle(red, green, blue, obstaclex, obstacley, size){
+// Draw moving obstacle
+function movingObstacle(red, green, blue, obstaclex, obstacley, size) {
     fill(red, green, blue);
     square(obstaclex, obstacley, size);
+}
+
+// Draw clicked obstacle
+function clickedObstacle(red, green, blue, mousex, mousey, width, height) {
+    fill(red, green, blue);
+    rect(mousex, mousey, width, height);
+}
+
+// Draw goal
+function makeGoal(red, green, blue, x, y, width, height) {
+    fill(red, green, blue);
+    rect(x, y, width, height);
+}
+
+// Move obstacle and check boundary
+function obstacleMove(leftBoundary, rightBoundary, topBoundary, bottomBoundary) {
+    if (obstaclex <= leftBoundary || obstaclex >= rightBoundary) {
+        movementx *= -1;
     }
 
-    function clickedObstacle(red, green, blue, mousex, mousey, width, height){
-        fill(red, green, blue);
-        rect(mousex, mousey, width, height);
-        }
+    if (obstacley <= topBoundary || obstacley >= bottomBoundary) {
+        movementy *= -1;
+    }
+
+    obstaclex += movementx;
+    obstacley += movementy;
+}
+
+// Move player with arrow keys
+function playerMove(speed) {
+    if (keyIsDown(37)) {
+        playerx -= speed;
+    } 
+    if (keyIsDown(39)) {
+        playerx += speed;
+    } 
+    if (keyIsDown(38)) {
+        playery -= speed; 
+    } 
+    if (keyIsDown(40)) {
+        playery += speed; 
+    }
+}
+
+// Check win condition
+function winCondition(leftBoundary, rightBoundary, topBoundary, bottomBoundary, textSize, textx, texty) {
+    if (playerx > leftBoundary && playerx < rightBoundary && playery > topBoundary && playery < bottomBoundary) {
+        movementx = 0;
+        movementy = 0;
+        textSize(textSize);
+        text('YOU WIN!', textx, texty);
+    }
+}
+
+// Draw border
+function makeBorder(red, green, blue, weight, value1, value2) {
+    fill(red, green, blue);
+    strokeWeight(weight);
+    line(value1, value1, value1, value2);
+    line(value1, value1, value2, value1);
+    line(value2, value1, value2, value2);
+    line(value1, value2, value2, value2);
+}
 
 function draw() {
     background(0);
-    
+
+    makeBorder(300, 300, 300, 10, 0, 600);
+
     player(173, 69, 22, playerx, playery, 25);
 
     movingObstacle(64, 127, 4, obstaclex, obstacley, 50);
 
-    movingObstacle(172, 86, 189,obstaclex,obstacley, 50);
-    
+    movingObstacle(172, 86, 189, obstaclex, obstacley, 50);
+
     clickedObstacle(188, 188, 74, mousex, mousey, 50, 100);
 
-    // Goal
-    fill (200, 200, 200);
-    rect (200, 10, 100, 50);
-    
-    // Movement and Boundary Check for Moving Obstacle
-    if (obstaclex <= 600 && obstaclex >= 0) {
-        movementx *= -1;
-    } 
-    if (obstaclex >= 600) {
-        obstaclex = 1;
-    } 
-    if (obstaclex <= 0) {
-        obstaclex = 599;
-    }
+    obstacleMove(0, 600, 0, 600);
 
-    obstaclex += movementx;
+    playerMove(5);
 
-    if (obstacley <= 600 && obstacley >= 0) {
-        movementy *= -1;
-    } 
-    if (obstacley >= 600) {
-        obstacley = 1;
-    } 
-    if (obstacley <= 0) {
-        obstacley = 599; 
-    }
-
-    obstacley += movementy;
-
-    // Player Movement
-    if (keyIsDown(37)) {
-        playerx -= 5;
-    } 
-    if (keyIsDown(39)) {
-        playerx += 5;
-    } 
-    if (keyIsDown(38)) {
-        playery -= 5; 
-    } 
-    if (keyIsDown(40)) {
-        playery += 5; 
-    }
+    winCondition(200, 400, 10, 50, 50, 300, 400);
 }
 
 function mouseClicked() {
     mousex = mouseX;
     mousey = mouseY;
-}
-
-// Win Condition
-if (200 < playerx > 100 && 50 < playery > 10){
-    movementx = 0;
-    movementy = 0;
-    obstaclex = 0;
-    obstacley = 0;
-    textSize (100);
-    text ('YOU WIN!', 300, 400);
 }
